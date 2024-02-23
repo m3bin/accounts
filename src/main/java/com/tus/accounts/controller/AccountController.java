6ae019cb41225524ad405fc0da07b807f354cd67
Mebin Mathew <30719660+m3bin@users.noreply.github.com>
2024-02-23 14:29:30 +0000

    added: update and delete api, exceptions, data validation, audit columns

--- src/main/java/com/tus/accounts/controller/AccountController.java
+++ src/main/java/com/tus/accounts/controller/AccountController.java
@@ -3,8 +3,11 @@ package com.tus.accounts.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,27 +18,63 @@ import com.tus.accounts.dto.CustomerDto;
 import com.tus.accounts.dto.ResponseDto;
 import com.tus.accounts.service.IAccountsService;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping(path = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
 @AllArgsConstructor
+@Validated
 public class AccountController {
-
-	@GetMapping("/account")
-	public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam String mobileNumber){
-		CustomerDto customerDto = iAccountsService.fetchAccount(mobileNumber);
-		return ResponseEntity.status(HttpStatus.OK).body(customerDto);
-	}
 	
 	private IAccountsService iAccountsService;
 
-	@PostMapping
-	public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+	@GetMapping("/account")
+	public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam 
+			@Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits") String mobileNumber){
+		CustomerDto customerDto = iAccountsService.fetchAccount(mobileNumber);
+		return ResponseEntity.status(HttpStatus.OK).body(customerDto);
+	}
+
+	@PostMapping("/account")
+	public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) {
 		iAccountsService.createAccount(customerDto);
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
 
 	}
+	
+	@PutMapping("/account")
+	public ResponseEntity<ResponseDto> updateAccountDetails(@Valid @RequestBody CustomerDto customerDto) {
+		boolean isUpdated = iAccountsService.updateAccount(customerDto);
+		if(isUpdated) {
+			return ResponseEntity
+					.status(HttpStatus.OK)
+					.body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
+		}
+		else {
+			return ResponseEntity
+					.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body(new ResponseDto(AccountsConstants.STATUS_500, AccountsConstants.MESSAGE_500));
+		}
+	}
+	
+	@DeleteMapping("/account")
+	public ResponseEntity<ResponseDto> deleteAccountDetails(@RequestParam
+			@Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits") String mobileNumber){
+		boolean isDeleted = iAccountsService.deleteAccount(mobileNumber);
+		if(isDeleted) {
+			return ResponseEntity
+					.status(HttpStatus.OK)
+					.body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
+		}
+		else {
+			return ResponseEntity
+					.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body(new ResponseDto(AccountsConstants.STATUS_500, AccountsConstants.MESSAGE_500));
+		}
+	}
+	
 
 }
--- src/main/java/com/tus/accounts/exception/GlobalExceptionHandler.java
+++ src/main/java/com/tus/accounts/exception/GlobalExceptionHandler.java
@@ -1,20 +1,44 @@
 package com.tus.accounts.exception;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.validation.FieldError;
+
 
 import com.tus.accounts.dto.ErrorResponseDto;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler{
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request){
+		Map<String, String> validationErrors = new HashMap<>();
+		List<ObjectError> validationErrorList = ex.getBindingResult().getAllErrors();
+		
+		validationErrorList.forEach((error) -> {
+			String fieldName = ((FieldError)error).getField();
+			String ValidationMsg = error.getDefaultMessage();
+			validationErrors.put(fieldName, ValidationMsg);
+		});
+		return new ResponseEntity<>(validationErrors,HttpStatus.BAD_REQUEST);
+	}
 	
 	@ExceptionHandler(CustomerAlreadyExistsException.class)
-	public ResponseEntity<ErrorResponseDto> handleCustomerAlreadyException(CustomerAlreadyExistsException exception, WebRequest webRequest){
+	public ResponseEntity<ErrorResponseDto> handleCustomerAlreadyExistsException(CustomerAlreadyExistsException exception, WebRequest webRequest){
 		
 		ErrorResponseDto errorResponseDTO = new ErrorResponseDto(
 				webRequest.getDescription(false),
@@ -35,6 +59,17 @@ public class GlobalExceptionHandler {
 				LocalDateTime.now()
 		);
 		return new ResponseEntity<>(errorResponseDTO, HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ErrorResponseDto> handleGlobalException(Exception exception, WebRequest webRequest){
+		ErrorResponseDto errorResponseDTO = new ErrorResponseDto(
+				webRequest.getDescription(false),
+				HttpStatus.INTERNAL_SERVER_ERROR,
+				exception.getMessage(),
+				LocalDateTime.now()
+		);
+		return new ResponseEntity<>(errorResponseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
 }
